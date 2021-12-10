4b3ee160e8d727da6c2e7fb23f56bc0a117d0eec
ayodiya <devayodiya@gmail.com>
2021-12-10 15:43:57 +0100

    remove unused route  and comment appropriately

--- routes/userMemoryVersesRoutes.js
+++ routes/userMemoryVersesRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express'
 
 import {
-  addMemoryVerse,
+
   getMemoryVerses
 } from '../controllers/UserMemoryVersesController.js'
 
 import { auth } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.post('/addMemoryVerse', auth, addMemoryVerse)
 router.get('/getMemoryVerses', auth, getMemoryVerses)
 
 export default router
